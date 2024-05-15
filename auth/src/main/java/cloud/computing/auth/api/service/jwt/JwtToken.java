6b0829e094499e50636810a334368f726930fd91
j-ra1n <j_ra1n@naver.com>
2024-05-15 23:42:05 +0900

    auth(#4): jwt, oauth setting

---
+++ auth/src/main/java/cloud/computing/auth/api/service/jwt/JwtToken.java
@@ -0,0 +1,18 @@
+package cloud.computing.auth.api.service.jwt;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class JwtToken {
+    private String accessToken;
+    private String refreshToken;
+
+    @Builder
+    public JwtToken(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}

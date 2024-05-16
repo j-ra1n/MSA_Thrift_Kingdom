ae5aa34757ac4c692b02dbe55c514a72bf825438
kooyeongmin <rn9dud0@naver.com>
2024-05-16 15:48:30 +0900

    url 네이밍 간소화

--- src/main/java/com/cloud/bbserver/controller/BoardController.java
+++ src/main/java/com/cloud/bbserver/controller/BoardController.java
@@ -18,14 +18,14 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    // 게시글 작성
-    @PostMapping("/write")
+    // Bulletin Board 게시글 작성
+    @PostMapping("/")
     public String boardWrite(@RequestBody BoardRequestDto board) {
         boardService.write(board);
-        return "글 작성이 완료되었습니다!";
+        return "Writing is complete";
     }
 
-    //게시글 리스트 목록
+    // Bulletin Board 게시글 리스트 목록
     @GetMapping("/list")
     public Page<Board> boardList(@PageableDefault(page = 0, size = 10, sort = "id",
             direction = Sort.Direction.DESC) Pageable pageable,
@@ -37,26 +37,26 @@ public class BoardController {
         }
     }
 
-    //게시글 조회
-    @GetMapping("/view/{id}")
+    // Bulletin Board 게시글 조회
+    @GetMapping("/{id}")
     public Board boardView(@PathVariable("id") Integer id) {
         return boardService.boardView(id);
     }
 
-    //게시글 삭제
-    @DeleteMapping("/delete/{id}")
+    // Bulletin Board 게시글 삭제
+    @DeleteMapping("/{id}")
     public String boardDelete(@PathVariable("id") Integer id) {
         boardService.boardDelete(id);
-        return "삭제완료!";
+        return "Deleted complete";
     }
 
-    // 게시글 수정
-    @PutMapping("/update/{id}")
+    // Bulletin Board 게시글 수정
+    @PutMapping("/{id}")
     public String boardUpdate(@PathVariable("id") Integer id, @RequestBody Board board) {
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
         boardService.boardUpdate(boardTemp);
-        return "게시글이 업데이트되었습니다!";
+        return "post has been updated";
     }
 }

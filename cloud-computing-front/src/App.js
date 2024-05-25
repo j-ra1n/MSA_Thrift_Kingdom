cb9834ef345557d4cceb6f27b8f7ba24186fc796
j-ra1n <j_ra1n@naver.com>
2024-05-26 00:24:34 +0900

    front(#1): App.js

---
+++ cloud-computing-front/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import './App.css';
+import Login from './login/Login';
+import Door from './component/Door';
+import BulletinBoard from './component/BulletinBoard';
+import SharingBoard from './component/SharingBoard';
+import PostDetail from './board/PostDetail';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const query = useQuery();
+
+  useEffect(() => {
+    if (query.get('loggedIn') === 'true') {
+      setIsLoggedIn(true);
+    }
+  }, [query]);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={!isLoggedIn ? <Login onLogin={handleLogin} /> : <Navigate to="/doors" />} />
+        <Route path="/doors" element={isLoggedIn ? <Door /> : <Navigate to="/" />} />
+        <Route path="/bulletin" element={isLoggedIn ? <BulletinBoard /> : <Navigate to="/" />} />
+        <Route path="/bulletin/:id" element={isLoggedIn ? <PostDetail /> : <Navigate to="/" />} />
+        <Route path="/sharing" element={isLoggedIn ? <SharingBoard /> : <Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;

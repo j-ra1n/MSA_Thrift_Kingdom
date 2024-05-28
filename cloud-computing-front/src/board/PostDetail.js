a0643ea4f02604943b3bcb49a6e0edaad786172c
j-ra1n <j_ra1n@naver.com>
2024-05-29 00:12:24 +0900

    front(#10): PostDetail.js refactor

--- cloud-computing-front/src/board/PostDetail.js
+++ cloud-computing-front/src/board/PostDetail.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './PostDetail.css';
+import Comment from './Comment';
 
 const PostDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchPost();
@@ -13,27 +15,42 @@ const PostDetail = () => {
 
   const fetchPost = () => {
     fetch(`http://localhost:8082/board/${id}`)
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then(data => setPost(data))
-      .catch(error => console.error('Error fetching post:', error));
+      .catch(error => {
+        console.error('Error fetching post:', error);
+        setError('게시글을 불러올 수 없습니다. 나중에 다시 시도해주세요.');
+      });
   };
 
   return (
     <div className="post-detail-container">
-      <button className="back-button" onClick={() => navigate(-1)}>←</button>
-      {post ? (
-        <>
-          <h1 className="post-title">{post.title}</h1>
-          <div className="post-meta">
-            <p className="post-writer">작성자: {post.nickname}</p>
-            <p className="post-date">{new Date(post.createdTime).toLocaleDateString()}</p>
-          </div>
-          <hr className="post-divider" />
-          <div className="post-content">{post.content}</div>
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div className="post-detail-content">
+        <button className="back-button" onClick={() => navigate(-1)}>←</button>
+        {error ? (
+          <p>{error}</p>
+        ) : (
+          post ? (
+            <>
+              <h1 className="post-title">{post.title}</h1>
+              <div className="post-meta">
+                <p className="post-writer">{post.nickname}</p>
+                <p className="post-date">{new Date(post.createdTime).toLocaleDateString()}</p>
+              </div>
+              <hr className="post-divider" />
+              <div className="post-content">{post.content}</div>
+            </>
+          ) : (
+            <p>Loading...</p>
+          )
+        )}
+      </div>
+      <Comment boardId={id} />
     </div>
   );
 };

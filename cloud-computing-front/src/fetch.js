a2e97ac9742590e1054ba69737d89ea3c281b408
j-ra1n <j_ra1n@naver.com>
2024-06-12 23:06:00 +0900

    front(#57): refactor

--- cloud-computing-front/src/fetch.js
+++ cloud-computing-front/src/fetch.js
@@ -2,7 +2,7 @@
 const BASE_URL = 'http://localhost:8090';
 //http://172.25.235.200:80
 
-const BB_BASE_URL = `${BASE_URL}/bb-server`;
+const BB_BASE_URL = `${BASE_URL}`;
 const CM_BASE_URL = `${BASE_URL}/comment`;
 const SB_BASE_URL = `${BASE_URL}/item`;
 const Login_BASE_URL = `${BASE_URL}/auth`

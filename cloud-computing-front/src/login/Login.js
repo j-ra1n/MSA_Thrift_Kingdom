d4f130298ec8829d7c9248fd94a913e5ff188377
j-ra1n <j_ra1n@naver.com>
2024-06-16 23:48:20 +0900

    front(#68)

--- cloud-computing-front/src/login/Login.js
+++ cloud-computing-front/src/login/Login.js
@@ -20,8 +20,7 @@ const Login = ({ onLogin }) => {
 
   const handleLogin = async (platform) => {
     try {
-      //const response = await fetch(`${Login_BASE_URL}/loginPage?platform=${platform}`);
-      const response = await fetch(`${Login_BASE_URL}/doors?loggedIn=true&nickname=정우`);
+      const response = await fetch(`${Login_BASE_URL}/loginPage?platform=${platform}`);
       const text = await response.text(); // 응답을 텍스트로 읽기
       console.log("Response text:", text); // 응답 텍스트 출력
       const data = JSON.parse(text); // 텍스트를 JSON으로 파싱

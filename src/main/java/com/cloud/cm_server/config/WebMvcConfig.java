8cfaccd92f753dd3497d363dbed8c161efeb0511
kooyeongmin <rn9dud0@naver.com>
2024-06-17 16:19:17 +0900

    [refactor] CORS 허용

--- src/main/java/com/cloud/cm_server/config/WebMvcConfig.java
+++ src/main/java/com/cloud/cm_server/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
